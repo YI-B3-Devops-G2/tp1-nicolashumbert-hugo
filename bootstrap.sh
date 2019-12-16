c462d478df246f46085273ce95b4a947332eb81e
twizou <57894503+twizou@users.noreply.github.com>
2019-12-16 17:18:50 +0100

    vagrant + bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+sudo apt -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
+sudo apt -y install openssh-server
+sudo apt -y install nginx
